5e7b0374c1222e85f8df38776f7c2ec346b447fe
Krzysztof  Jamrog <4jamrog@stud112>
2017-10-19 19:35:33 +0200

    simulation

--- kol1.py
+++ kol1.py
@@ -13,3 +13,44 @@
 #Do your best, show off with good, clean, well structured code - this is more important than number of features.
 #After you finish, be sure to UPLOAD this (add, commit, push) to the remote repository.
 #Good Luck
+
+import random
+import time
+
+def getRandomOrientation():
+    return random.gauss(0, 20)
+
+print('Simulating turbulations.\nPress Ctrl^C to break\n')
+
+class Correction(object):
+    def __init__(self):
+        self.start_orientation = random.gauss(0, 10)
+        print('Start orientation: ' + str(self.start_orientation))
+        self.current_orientation = self.start_orientation
+    
+    # Setting correction to keep initial orientation
+    def updateCorrection(self, turbulence):
+        correction = -turbulence
+        # Prevent to set correction greater than 6 or lower than -6
+        if correction > 6:
+            correction = 6
+        elif correction < -6:
+            correction = -6
+        self.current_orientation = self.start_orientation + turbulence + correction
+        print('correction ' + str(correction))
+
+    def printActual(self):
+        print('Actual orientation: ' + str(self.current_orientation))
+
+
+correction = Correction()
+
+while True:
+    try:
+        turbulence = random.gauss(0, 5)
+        correction.updateCorrection(turbulence)
+        correction.printActual()
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print('Simulation ended')
+        break
